45e07ae1cdea0ac2f521847ce9173817c6f17659
angga399 <saputra.angga.qi@gmail.com>
2025-09-12 16:34:01 +0700

    Coral

--- scripts/xml_to_csv.py
+++ scripts/xml_to_csv.py
@@ -2,17 +2,17 @@ import csv
 import re
 
 # [UBAH SESUAI NAMA FILE XML] 
-with open("EN_Bonebottom.xml", "r", encoding="utf-8") as f:
+with open("EN_Coral.xml", "r", encoding="utf-8") as f:
     data = f.read()
 
 entries = re.findall(r'<entry name="(.*?)">(.*?)</entry>', data, re.DOTALL)
 
 # [UBAH SESUAI NAMA FILE CSV]
-with open("EN_Bonebottom.csv", "w", newline="", encoding="utf-8") as f:
+with open("EN_Coral.csv", "w", newline="", encoding="utf-8") as f:
     writer = writer = csv.writer(f, delimiter=";")
     writer.writerow(["name", "original_text", "translated_text"])
     for name, text in entries:
         writer.writerow([name, text, ""])
 
 # [UBAH SESUIAI NAMA FILE CSV]
-print("✅ Extract selesai! Cek file EN_Bonebottom.csv")
+print("✅ Extract selesai! Cek file EN_Coral.csv")
